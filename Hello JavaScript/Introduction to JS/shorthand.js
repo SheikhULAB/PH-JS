e4848b058342fdd6a1d7202dfe18014b277d7104
SheikhULAB <tosibalaptop86@gmail.com>
2023-08-11 17:53:18 +0600

    shorthand

---
+++ Hello JavaScript/Introduction to JS/shorthand.js
@@ -0,0 +1,47 @@
+var tomatoPrice = 25; //variable declaration
+tomatoPrice = 30; // variable value update
+
+// add something to the prvious value
+
+var price1 = 31;
+var price2 = 10;
+
+var price1 = 35; //set a new value
+
+price1 = price1 + 10; //add some value to the existing value
+// console.log(price1);
+
+// addtion to the same variable shorthand
+price1 += 10; //adding some value to the exising value shorthand
+// console.log(price1);
+
+price2 = price2 -5;
+// console.log(price2);
+
+price2 -= 5;
+console.log(price2); //shorthand
+
+
+price1 = price1 * 5;
+console.log(price1);
+
+price1 *= 5;
+console.log(price1);
+
+price1 = price1 /5;
+console.log(price1);
+
+price1 /= 5;
+console.log(price1);
+
+var age = 15;
+age = age + 1;
+console.log(age);
+age++; //add 1
+console.log(age);
+
+var love = 100;
+love = love -1;
+console.log(love);
+love--;//sub 1
+console.log(love);
