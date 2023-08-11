263dd013f3e75ff6819b3fbe7db85e45ad8bba9d
SheikhULAB <tosibalaptop86@gmail.com>
2023-08-11 18:28:53 +0600

    reminder

---
+++ Hello JavaScript/Introduction to JS/reminder.js
@@ -0,0 +1,11 @@
+var mangos = 7;
+
+var person = 2;
+
+// / this means vagfol
+// % this means vagsesh reminder
+// % knows as a modulas operator
+
+console.log(mangos / person); // 3.5 vagfol
+
+console.log(mangos % person); // 1 vagsesh reminder
