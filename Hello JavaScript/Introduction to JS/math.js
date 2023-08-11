9e2eda7a1f1de9f7113b407997164d30a84047ea
SheikhULAB <tosibalaptop86@gmail.com>
2023-08-11 15:55:11 +0600

    basic math needed for programming

---
+++ Hello JavaScript/Introduction to JS/math.js
@@ -0,0 +1,17 @@
+// basic math needed to learn for programming
+
+// 1.Number ==> 1,2,55,-32,-1,0
+// 2.Integer: 2,4,5
+// 3.Float: 2.5, 13.67,32.69
+// 4.Operation: additon,subtraction,multipy,divison,remainder
+// 5.Percent
+// 6.Average
+// 7.Comparison: >,<,==,!=
+// 8.Oikik
+// 9.45,25,68,68,129,2: max,min,average,sum
+// 10.series
+
+// extra:
+
+// 1.Prime Number
+// 2.permutation
