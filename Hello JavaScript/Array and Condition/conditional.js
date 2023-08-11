0ba622e9006c50b708cabfef48d8673f5793c071
SheikhULAB <tosibalaptop86@gmail.com>
2023-08-12 00:30:55 +0600

    conditional

---
+++ Hello JavaScript/Array and Condition/conditional.js
@@ -0,0 +1,26 @@
+var iphonePrice = 79000;
+var myBudget = 4500;
+
+if(iphonePrice < myBudget) {
+    console.log("i will buy the iPhone");
+} else {
+    console.log("i will not but the phone"); //this will print out
+}
+
+myBudget = myBudget + 78000;
+
+if(iphonePrice < myBudget) {
+    console.log("i will buy the iPhone"); //this will print out
+} else {
+    console.log("i will not but the phone");
+}
+
+var chickenPrice = 500;
+var myMoney = 50;
+
+if(chickenPrice <= myMoney) {
+    console.log("murgir run kahmu and haddi chimanu");//this will not print out
+} else {
+    console.log("alur vorta and dail khamu")// will print
+}
+
