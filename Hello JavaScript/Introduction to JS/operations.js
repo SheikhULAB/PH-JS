d62165ff43ab70bdc4b853710a34042b5eaf1690
SheikhULAB <tosibalaptop86@gmail.com>
2023-08-11 17:02:32 +0600

    simple math operations

---
+++ Hello JavaScript/Introduction to JS/operations.js
@@ -0,0 +1,26 @@
+var onionPrice = 20;
+var eggPrice = 10;
+
+// console.log(onionPrice + eggPrice);
+//additions
+var totalPrice = onionPrice + eggPrice;
+// console.log(totalPrice);
+
+//substraction
+var priceDifference = onionPrice - eggPrice;
+// console.log(priceDifference);
+
+var orangePrice = 20;
+var quantity = 7;
+
+//multiplicatin
+var totalCost = orangePrice * quantity;
+// console.log(totalCost);
+
+//division
+var money = 500;
+var player = 11;
+
+var eachPlayer = money / player;
+// console.log(eachPlayer);
+
