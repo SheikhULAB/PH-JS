73f2eb189642c42cb61eb3aa536c4f8e7b5189af
SheikhULAB <tosibalaptop86@gmail.com>
2023-08-11 16:09:25 +0600

    variable

---
+++ Hello JavaScript/Introduction to JS/variable.js
@@ -0,0 +1,12 @@
+// vary + able = variable
+
+// a variable is a value that can change,depending on
+// condtion or on information passed to the program.
+
+var price = 13;
+
+// tips: a variable declared without a value will have the value undefined
+
+var age = 17;
+
+var temperature = 38;
