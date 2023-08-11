4645086921e02484071015e69f1fd03b9419233f
SheikhULAB <tosibalaptop86@gmail.com>
2023-08-11 16:45:17 +0600

    variable naming conventions

---
+++ Hello JavaScript/Introduction to JS/variable-name.js
@@ -0,0 +1,32 @@
+var price = 29;
+
+// JavaScript keyword: js keywords are reserved words. keywords cannot be
+// used as variables names , functions names etc in our js programs.
+// Keyword are case sensetive
+
+// variables name is also case sensetive
+//    var person = 25;
+//    var Person = 35; these ar 2 different variables
+
+// 1. variable can not be any keywords(reserve words)
+    //   var false = 96; lat batti
+    //   var return = true; lal batti
+
+// 2. a variable name should be in one word. no spacing allowed
+//   var my home address = "new california"; lal batti
+
+// 3. a variable cannot have quotation
+//    var "name" = "Tom hanks"; lat batti
+
+// 4. a variable cannot start with a number but can ends with a number
+// var 99club = 1964; lal batti
+// var club25 = 2025; it is allowed
+
+// how to use long names
+// 5. can not use dash
+//   var user-name = "raj bappa"; lal batti
+//  var userName = "raj bappa"; camel case used in js
+// var user_home_addres = "bandorban";snake case. undercase is allowed but used in python
+// var UserHomeAddress = "dhaka"; pascal case
+
+// we use the camelCase in JS
