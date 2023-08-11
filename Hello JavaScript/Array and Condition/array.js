dbd52af9ddfff40dfac3970dbacc2e1144313e61
SheikhULAB <tosibalaptop86@gmail.com>
2023-08-11 22:50:01 +0600

    array

---
+++ Hello JavaScript/Array and Condition/array.js
@@ -0,0 +1,16 @@
+var friendsAge = [11, 12, 14, 10, 56];
+
+console.log(friendsAge); //[ 11, 12, 14, 10, 56 ]
+console.log(friendsAge.length);//5
+
+var picnicFee = [5000, 2000, 4000, 150];
+console.log(picnicFee); //[ 5000, 2000, 4000, 150 ]
+console.log(picnicFee.length);// 4
+
+var nayikas = ["mahi", "opu biswas", "sabnoor", "Sabana"];
+console.log(nayikas); //[ 'mahi', 'opu biswas', 'sabnoor', 'Sabana' ]
+console.log(nayikas.length); // 4
+
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+console.log(vowels); //[ 'a', 'e', 'i', 'o', 'u' ]
+console.log(vowels.length); // 5
