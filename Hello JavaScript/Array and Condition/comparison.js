ea97dc3e7483f52d5c2f347ef5305ecfe312a776
SheikhULAB <tosibalaptop86@gmail.com>
2023-08-12 00:19:52 +0600

    comparison

---
+++ Hello JavaScript/Array and Condition/comparison.js
@@ -0,0 +1,30 @@
+console.log(5 < 6);//true
+console.log(5 > 6);//false
+
+console.log(5==6);//false
+console.log(6==6);//true
+console.log(6!=5);//true
+console.log(6!=6);//false
+
+console.log(5 <=6);//true
+console.log(5 <=5);//true
+console.log(5 <5);//false
+
+console.log(5 >=6);//false
+console.log(5 >=5);//true
+console.log(5 >5);//false
+
+var num1 = 56;
+var num2 = 89;
+console.log(num1 < num2); //true
+console.log(num1 <= num2); //true
+console.log(num1 > num2); //false
+console.log(num1 >= num2); //false
+console.log(num1 === num2); //false
+console.log(num1 !== num2); //true
+
+//multiple condition
+
+money1 > money2 && result1 > result2 && height1 > height2 // all nedd to be true
+
+money1 > money2 || result1 > result2 || height1 > height2//only 1 need to be true
