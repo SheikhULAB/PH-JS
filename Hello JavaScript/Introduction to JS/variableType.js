8009e55273c04ffdda49466700b06bb0d2750e8d
SheikhULAB <tosibalaptop86@gmail.com>
2023-08-11 16:25:18 +0600

    variable types

---
+++ Hello JavaScript/Introduction to JS/variableType.js
@@ -0,0 +1,24 @@
+
+var person = "sodor uddin"; //characters //string
+
+var location = "andor killa bandorban"; //using double quotation
+
+var special = 'alia bhat'; // using single quotation
+
+//String data types: zero or more characters written inside quotes
+
+// Numeric data types: numbers can be written with or without
+//decimals and without quotes.
+var price = 11;
+var age = 26;
+
+//Boolean data types: it can only take the values true or false
+//and written without quotes. 0 or 1
+var serious = true;
+
+var isFullMarks = true;
+
+var isSingle = false;
+
+// We can think variable as a box where we can store things.
+// there is others data types in js like undefined and null.
