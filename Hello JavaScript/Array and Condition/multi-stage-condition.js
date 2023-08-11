8e4d8083690ab06f4b0b729b18d7897de75fcf17
SheikhULAB <tosibalaptop86@gmail.com>
2023-08-12 01:57:18 +0600

    multi stage condition and nested condition

---
+++ Hello JavaScript/Array and Condition/multi-stage-condition.js
@@ -0,0 +1,34 @@
+var money = 10;
+
+var danishPrice = 45;
+
+var butterBread = 35;
+
+var toastBiscuit = 20;
+
+if(danishPrice < money) {
+    console.log("dan dan danish khamu");
+} 
+else if(butterBread < money){
+    console.log("butterban kahabo");
+}
+else if(toastBiscuit < money) {
+    console.log("cubai cubai toastBiscuite kabo");
+} else {
+    console.log("1 cup cha khabo");//this is print out
+}
+
+money = 30;
+
+
+if(danishPrice < money) {
+    console.log("dan dan danish khamu");
+} 
+else if(butterBread < money){
+    console.log("butterban kahabo");
+}
+else if(toastBiscuit < money) {
+    console.log("cubai cubai toastBiscuite kabo");//this is print out
+} else {
+    console.log("1 cup cha khabo");
+}
---
+++ Hello JavaScript/Array and Condition/nested-condition.js
@@ -0,0 +1,19 @@
+var math = true;
+var geometry = true;
+var straightLine = false;
+
+if(math == true) {
+    if(geometry == true){
+      if(straightLine == true) {
+       console.log("straight line")
+      }else {
+        console.log("baka pothe colba nah");//this is print out
+      }
+    }
+    else {
+        console.log("pithagoras hoite parba nah")
+    }
+  
+}  else{
+    console.log("not good at math");
+}
