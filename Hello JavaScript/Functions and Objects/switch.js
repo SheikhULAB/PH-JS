1954edf2a5a3d1edf1cbe05355174fe5ef420791
SheikhULAB <tosibalaptop86@gmail.com>
2023-08-13 03:14:24 +0600

    switch case

---
+++ Hello JavaScript/Functions and Objects/switch.js
@@ -0,0 +1,21 @@
+const color = 'yellow';
+
+switch(color){
+    case 'green':
+        console.log("green friend");
+        break;
+    case 'blue':
+        console.log("blue friend");
+        break;
+    case 'white':
+        console.log("whie friend");
+        break;
+    case 'yellow':
+        console.log("himi friend");
+        break;
+    case 'black':
+        console.log("black friend");
+        break;
+    default:
+        console.log("you are not my friend");                    
+}
