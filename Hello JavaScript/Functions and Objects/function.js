766d3a09896f93ac34de60bb0d68e8bfcccda744
SheikhULAB <tosibalaptop86@gmail.com>
2023-08-12 23:51:39 +0600

    function and parameter

---
+++ Hello JavaScript/Functions and Objects/function.js
@@ -0,0 +1,16 @@
+//function declaration
+function startFan(){
+    console.log("walk to the switch");
+    console.log("start fan");
+}
+//call the function
+startFan();
+// it will print 
+// walk to the switch
+// start fan
+startFan();//again it will print the same
+
+console.log("waking up to the morning");
+console.log("watching js tutorial");
+
+startFan();
