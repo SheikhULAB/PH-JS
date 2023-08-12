f3f692eb4fd9244fe421c86e58c41e5cd7536740
SheikhULAB <tosibalaptop86@gmail.com>
2023-08-12 17:41:05 +0600

    reverse

---
+++ Hello JavaScript/Loops/reverse.js
@@ -0,0 +1,21 @@
+var num = 10;
+
+while (num >= 1) {
+    console.log(num);
+    num--;
+}
+// output: 
+// 10
+// 9
+// 8
+// 7
+// 6
+// 5
+// 4
+// 3
+// 2
+// 1
+
+for(var i = 10; i >=1; i--) {
+    console.log(i);// 10 to 1
+}
