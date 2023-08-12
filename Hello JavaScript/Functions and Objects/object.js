3a08a704a5e55d7b35b4691152b6fbe8228e58a5
SheikhULAB <tosibalaptop86@gmail.com>
2023-08-13 01:58:44 +0600

    object

---
+++ Hello JavaScript/Functions and Objects/object.js
@@ -0,0 +1,28 @@
+const student = {
+    id: 1,
+    name: "solaiman khan",
+    class: 9,
+    marks: 98,
+}
+console.log(student);
+
+
+const mobile = {
+    brand: 'samsung',
+    price: 19000,
+    storage: '64gb',
+    camera: '7MP',
+}
+console.log(mobile);
+console.log(mobile.brand);// samsung
+
+
+const myComputer = {
+    brand: 'lenovo',
+    price: '39000',
+    color: 'silver',
+    processor: 'i7',
+}
+console.log(myComputer);
+console.log(myComputer.price);// 39000
+
