3872958a0895cd627ec136464e017a303e02bb7b
SheikhULAB <tosibalaptop86@gmail.com>
2023-08-12 16:41:19 +0600

    array-loop

---
+++ Hello JavaScript/Loops/array-loop.js
@@ -0,0 +1,14 @@
+var numbers = [45, 87, 89, 56, 32, 51, 25];
+//loop an array
+for(i = 0; i < numbers.length; i++) {
+    // console.log(i); // 0 to 6
+    console.log(numbers[i]);
+}
+// output: 
+// 45
+// 87
+// 89
+// 56
+// 32
+// 51
+// 25
