2da7a67c71befd903575eba55a2f6d316c9a03ff
SheikhULAB <tosibalaptop86@gmail.com>
2023-08-12 16:29:37 +0600

    for

---
+++ Hello JavaScript/Loops/for.js
@@ -0,0 +1,51 @@
+for(var roastGiven = 0; roastGiven < 7; roastGiven++) {
+    console.log("roast den vai");//print 7 times
+}
+
+//simple version of for loop
+
+for(var i = 0; i < 7; i++){
+    console.log("roast den vai");//print 7 times
+    console.log(i);// 0 to 6
+}
+
+
+for (var i = 1; i <= 10; i++) {
+    console.log(i); // 1 to 10
+}
+
+
+for (var i = 0; i <=20; i+=2){
+    console.log(i);
+}
+// output: 
+// 0
+// 2
+// 4
+// 6
+// 8
+// 10
+// 12
+// 14
+// 16
+// 18
+// 20
+
+
+
+
+for (var i = 1; i <=20; i+=2){
+    console.log(i);
+}
+
+// output: 
+// 1
+// 3
+// 5
+// 7
+// 9
+// 11
+// 13
+// 15
+// 17
+// 19
