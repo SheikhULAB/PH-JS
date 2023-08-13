a247bb3b5dad4b92d1ef88e835bc3f6869f979ea
SheikhULAB <tosibalaptop86@gmail.com>
2023-08-13 22:53:25 +0600

    recap

---
+++ Hello JavaScript/Apply JS Concept/recap.js
@@ -0,0 +1,34 @@
+var myName = 'Khan';
+1.variable
+
+math operation +,-,*,/
+shorthand: +=,--,++
+
+2.array
+
+var friends= ['abul','babul','dabul','kabul'];
+
+array postions knows as index 
+
+my thirdFriend = friends[2];
+friends[3] = 'dambool';
+
+3.conditonals
+if() {
+    >,<,<=,>=,==,===,!=,!==
+}
+
+else if() {
+
+}
+else {
+
+}
+
+4.loop
+while,for,for in
+
+4.function name(params) {
+    
+}
+5.objects
