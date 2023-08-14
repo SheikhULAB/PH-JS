22e0b900cfe0a218a1694eaa6bed1055fadfaead
SheikhULAB <tosibalaptop86@gmail.com>
2023-08-14 16:07:42 +0600

    math

---
+++ Hello JavaScript/JS Coding Problems/math.js
@@ -0,0 +1,27 @@
+const result = Math.pow(3, 4);
+console.log(result);//81
+
+const num1 = 25;
+const num2 = 45;
+
+const gap = Math.abs(num1 - num2);
+
+if(gap < 5) {
+    console.log("you guys can be friends");
+} else {
+    console.log("stay apart");//stay apart
+}
+
+const number = 2.4598;
+const fullNumber = Math.round(number);
+console.log(fullNumber);//2
+
+const ceil = Math.ceil(number);
+console.log(ceil);//3
+
+const floor = Math.floor(number);
+console.log(floor);//2
+
+// ...random....
+
+console.log(Math.round((Math.random() * 10)));
