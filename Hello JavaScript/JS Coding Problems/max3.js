f2e260efadf59a2f7f99b0dc0dcd9b119f5a52e5
SheikhULAB <tosibalaptop86@gmail.com>
2023-08-14 17:21:29 +0600

    max

---
+++ Hello JavaScript/JS Coding Problems/max3.js
@@ -0,0 +1,14 @@
+const jim = 84;
+const dela = 99;
+const chinku = 77;
+
+if(jim > dela && jim > chinku){
+    console.log("jim will get the cake");
+} else if(dela > jim && dela > chinku) {
+    console.log("dela will get the cake");//print out
+} else {
+    console.log("chinku will get the cake");
+}
+
+console.log(Math.max(45,89,55));//89
+console.log(Math.min(45,89,55));//45
