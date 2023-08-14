798e19590b43af29d50490ac0de2b768936cc8de
SheikhULAB <tosibalaptop86@gmail.com>
2023-08-14 18:07:24 +0600

    fibonacci

---
+++ Hello JavaScript/JS Coding Problems/fibo.js
@@ -0,0 +1,17 @@
+function fibonacci(n) {
+    if (n <= 0) {
+        return [];
+    } else if (n === 1) {
+        return [0];
+    } else if (n === 2) {
+        return [0, 1];
+    } else {
+        const fibSeries = fibonacci(n - 1);
+        fibSeries.push(fibSeries[fibSeries.length - 1] + fibSeries[fibSeries.length - 2]);
+        return fibSeries;
+    }
+}
+
+const n = 10; // Change this to the number of Fibonacci numbers you want
+const fibSeries = fibonacci(n);
+console.log(fibSeries); // Output: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
