0ff4b9cd8f9fb17be98eae8208a9231dbfe8010a
SheikhULAB <tosibalaptop86@gmail.com>
2023-08-16 01:09:25 +0600

    traver dom getElementsByTagName

--- Integrate JavaScript/tour of dom/js/dom.js
+++ Integrate JavaScript/tour of dom/js/dom.js
@@ -1,3 +1,13 @@
 console.log("hello from js");
 console.log(document);
 console.log(document.body);
+
+const h1Collection = document.getElementsByTagName('h1');
+const liCollection = document.getElementsByTagName('li');
+console.log(h1Collection);
+console.log(liCollection);
+
+for(const li of liCollection){
+    console.log(li);
+    console.log(li.innerText);
+}
